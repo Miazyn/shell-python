37b0ffeea8c0fbb78ff49634d3d733e201b4aed7
Miazyn <michelle.ma.fischer@gmail.com>
2024-06-10 17:03:32 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -18,7 +18,15 @@ def main():
             if shell_command in ['echo', 'type', 'exit']:
                 print(f"{shell_command} is a shell builtin")
             else:
-                print(f"{shell_command} not found")
+                found = False
+                for path in os.environment.get('PATH', '').split(':'):
+                    executable_path = os.path.join(path, shell_command)
+                    if os.path.isfile(executable_path) and os.access(executable_path, os.X_OK):
+                        print(f"{shell_command} is {executable_path}")
+                        found = True
+                        break
+                if not found:
+                    print(f"{shell_command} not found")
         else:
             sys.stdout.write(f"{command}: command not found\n")
 
