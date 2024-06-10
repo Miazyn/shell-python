17f9a2052efe34808d7d60fa08366c4fc1db7e29
Miazyn <michelle.ma.fischer@gmail.com>
2024-06-10 16:15:57 +0200

    pass stage

--- app/main.py
+++ app/main.py
@@ -6,8 +6,9 @@ def main():
     sys.stdout.flush()
 
     # Wait for user input
-    input()
+    command = input()
 
+    sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
